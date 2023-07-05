75741fe734421116247065a73e46dd22b18e2c92
Rithal <rithal@Cmiles-Mac-mini-2.local>
2023-07-05 19:12:08 +0530

    dashboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:meet_well/main/home_screen/home_screen.dart';
 import 'package:meet_well/main/provider/main_provider.dart';
-<<<<<<<<< Temporary merge branch 1
-import 'package:meet_well/screens/dashboard/dashboard_screen.dart';
-import 'package:meet_well/screens/login/controller/login_controller.dart';
+import 'package:meet_well/screens/dashboard_screen/controller/dashboard_screen_controller.dart';
+import 'package:meet_well/screens/dashboard_screen/view/dashboard_screen_view.dart';
 import 'package:meet_well/screens/login/view/login_screen.dart';
+import 'package:meet_well/screens/profile_details_screen/view/profile_details_screen_view.dart';
 import 'package:meet_well/screens/splash/view/splash_screen.dart';
 import 'package:meet_well/utils/constants/string_constants.dart';
-=========
-import 'package:meet_well/screens/Dashboard_Screen/controller.dart';
-import 'package:meet_well/screens/Dashboard_Screen/view.dart';
-import 'package:meet_well/screens/group_details_screen/view.dart';
-import 'package:meet_well/screens/notification_screen/view.dart';
-import 'package:meet_well/screens/profile_details_screen/view.dart';
-import 'package:meet_well/screens/splash/view/splash_screen.dart';
-import 'package:meet_well/screens/transaction_screen/view.dart';
->>>>>>>>> Temporary merge branch 2
 import 'package:meet_well/utils/route/route.dart' as routes;
-import 'package:meet_well/utils/route/route.dart';
 import 'package:provider/provider.dart';
-import 'main/navigation/bottomNavigationController.dart';
-
 import 'screens/splash/controller/splash_screen_controller.dart';
 import 'utils/route/route.dart';
 
@@ -45,27 +33,15 @@ class MyApp extends StatelessWidget {
         ),
         Provider<SplashScreenController>(
             create: (_) => SplashScreenController()),
-
-        ChangeNotifierProvider<LoginController>(
-          create: (context) => LoginController(),
+        ChangeNotifierProvider<DashboardScreenController>(
+          create: (context) => DashboardScreenController(),
         ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: StringConstant.boolFalse,
-        initialRoute: routes.splash,
-
-        ///Routes
-        routes: <String, WidgetBuilder>{
-          login: (BuildContext context) => LoginScreen(),
-          dashboard: (BuildContext context) => DashboardScreen()
-
-
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          fontFamily: 'poppins',
-          useMaterial3: true,
+          fontFamily: StringConstant.textFontFamilyPoppins,
+          useMaterial3: StringConstant.boolTrue,
           primarySwatch: Colors.blue,
         ),
 
@@ -73,12 +49,10 @@ class MyApp extends StatelessWidget {
 
         ///Routes
         routes: {
-          routes.dashboard: (context) => const Dashboard(),
-          routes.transactions: (context) => const Transaction(),
-          routes.notifications: (context) => const Notifications(),
-          routes.groupDetails: (context) => const Group_details(),
-          routes.profileScreen: (context) => const Profile_details(),
-
+          routes.dashboard: (context) => const DashboardScreen(),
+          routes.profileScreen: (context) => const ProfileDetailsScreen(),
+          routes.homeScreen: (context) => const HomeScreen(),
+          login: (BuildContext context) => LoginScreen(),
         },
 
         /// Passing Data as Arguments to screens
